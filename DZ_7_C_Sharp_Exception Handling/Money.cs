f48a4f68ad1acdf352ebdfc3c244c039a6624263
Дмитрий <@ADMIN>
2023-11-10 18:03:33 +0500

    Добавьте файлы проекта.

---
+++ DZ_7_C_Sharp_Exception Handling/Money.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+/*
+Написать класс Money, предназначенный для хранения денежной суммы (в рублях и копейках). Для класса
+реализовать перегрузку операторов + (сложение денежных сумм), – (вычитание сумм), / (деление суммы на целое
+число), * (умножение суммы на целое число), ++ (сумма
+увеличивается на 1 копейку), -- (сумма уменьшается на 1 копейку), <, >, ==, !=.
+Класс не может содержать отрицательную сумму.
+В случае если при исполнении какой-либо операции получается отрицательная сумма денег, то класс генерирует
+исключительную ситуацию «Банкрот».
+Программа должна с помощью меню продемонстрировать все возможности класса Money.
+Обработка исключительных ситуаций производится в программе.
+*/
+namespace DZ_7_C_Sharp_Exception_Handling
+{
+    internal class Money
+    {
+        private int rubles;
+        private int kopeks;
+        public int Rubles
+        {
+        get { return rubles; }
+            set
+            {
+            if(value < 0)
+                throw new BankruptException();
+            Rubles = value;
+            } 
+        }
+        public int Kopeks 
+        {
+            get { return kopeks; }
+            set { 
+            if( value < 0)
+                    throw new BankruptException();
+                kopeks = value % 100;
+                rubles += value / 100;
+            }
+        }
+        public Money(int rubles, int kopeks)
+        {
+            this.rubles = rubles;
+            this.kopeks = kopeks;
+        }
+
+        public Money() { Rubles = 0; Kopeks = 0; }
+
+        public static Money operator + (Money m1, Money m2)
+        {
+            return new Money(m1.Rubles + m2.Rubles, m1.Kopeks + m2.Kopeks);
+        }
+        public static Money operator - (Money m1, Money m2)
+        {
+            int totalKopeks =(m1.Rubles * 100 + m1.Kopeks) - (m2.Rubles * 100 + m2.Kopeks);
+            if(totalKopeks < 0)            
+                throw new BankruptException();
+                return new Money(totalKopeks / 100, totalKopeks % 100);          
+        }
+
+        public static Money operator / (Money money, int divisor)
+        {
+            if(divisor  == 0)
+                throw new DivideByZeroException();
+            return new Money(money.Rubles/ divisor, money.Kopeks/ divisor);
+        }
+
+        public static Money operator * (Money money, int miltiplier)
+        {
+            int totalKopeks = money.Rubles * 100 + money.Kopeks * miltiplier;
+            int newRubles = totalKopeks / 100;
+            int newKopeks = totalKopeks % 100;
+
+            return new Money(newRubles, newKopeks);
+        }
+
+        public static Money operator ++ (Money money)
+        {
+            return new Money(money.Rubles,money.Kopeks + 1);
+        }
+
+        public static Money operator -- (Money money)
+        {
+            return new Money(money.Rubles, money.Kopeks - 1);
+        }
+
+        public static bool operator > (Money m1 , Money m2 )
+        {
+            return (m1.Rubles * 100 + m1.Kopeks) > (m2.Rubles *100 + m2.Kopeks);
+        }
+
+        public static bool operator < (Money m1, Money m2)
+        {
+            return (m1.Rubles * 100 + m1.Kopeks) < (m2.Rubles * 100 + m2.Kopeks);
+        }
+
+        public static bool operator == (Money m1, Money m2)
+        {
+            return (m1.Rubles * 100 + m1.Kopeks) == (m2.Rubles * 100 + m2.Kopeks);
+        }
+
+        public static bool operator !=(Money m1, Money m2)
+        {
+            return (m1.Rubles * 100 + m1.Kopeks) != (m2.Rubles * 100 + m2.Kopeks);
+        }
+
+        public override string ToString()
+        {
+            return $"{Rubles} руб. {Kopeks} коп.";
+        }
+
+        public class BankruptException : Exception
+        {
+            public BankruptException() : base("Банкрот") { }
+        }
+    }
+}
---
+++ DZ_7_C_Sharp_Exception Handling/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ Написать класс Money, предназначенный для хранения денежной суммы (в рублях и копейках). Для класса
+реализовать перегрузку операторов + (сложение денежных сумм), – (вычитание сумм), / (деление суммы на целое
+число), * (умножение суммы на целое число), ++ (сумма
+увеличивается на 1 копейку), -- (сумма уменьшается на 1 копейку), <, >, ==, !=.
+Класс не может содержать отрицательную сумму.
+В случае если при исполнении какой-либо операции получается отрицательная сумма денег, то класс генерирует
+исключительную ситуацию «Банкрот».
+Программа должна с помощью меню продемонстрировать все возможности класса Money.
+Обработка исключительных ситуаций производится в программе.
+ */
+
+using static DZ_7_C_Sharp_Exception_Handling.Money;
+
+namespace DZ_7_C_Sharp_Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                Money m1 = new Money(10, 50);
+                Money m2 = new Money(5, 25);
+
+                Money sum = m1 + m2;
+                Console.WriteLine($"Сумма: {sum}");
+
+                Money diff = m1 - m2;
+                Console.WriteLine($"Разность: {diff}");
+
+                Money div = sum / 2;
+                Console.WriteLine($"Деление: {div}");
+
+                Money mul = sum * 3;
+                Console.WriteLine($"Умножение: {mul}");
+
+                Money inc = ++m1;
+                Console.WriteLine($"Увеличение: {inc}");
+
+                Money dec = --m1;
+                Console.WriteLine($"Уменьшение: {dec}");
+
+                if (m1 < m2)
+                    Console.WriteLine($"{m1} меньше, чем {m2}");
+
+                if (m1 > m2)
+                    Console.WriteLine($"{m1} больше, чем {m2}");
+
+                if (m1 == m2)
+                    Console.WriteLine($"{m1} равно {m2}");
+
+                if (m1 != m2)
+                    Console.WriteLine($"{m1} не равно {m2}");
+
+                // Генерация исключительной ситуации Банкрот
+                Money bankrupt = new Money(-10, 0);
+            }
+            catch (BankruptException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
